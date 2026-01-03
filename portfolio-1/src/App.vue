1277169f6a539e881af8f70636eef9f5d131ccec
unknown <Gideonayao@gmail.com>
2026-01-03 13:53:07 +0800

    Add : layout for projects

--- portfolio-1/src/App.vue
+++ portfolio-1/src/App.vue
@@ -18,18 +18,27 @@ import UnderDeveloped from './components/UnderDeveloped.vue';
     <SkillsComponent />
 
 
-    <div  id="projects" class="h-full bg-gradient-to-b from-gray-900 to-zinc-950 py-20 ">
+    <div id="projects" class="h-full bg-gradient-to-b from-gray-900 to-zinc-950 py-20 ">
       <div class="container mx-auto px-10">
         <h2 class="text-3xl font-bold text-white mb-6 text-center uppercase">Projects</h2>
         <div class="grid md:grid-cols-2  grid-cols-1 w-full gap-10">
-          <div class="w-full h-60 bg-white p-5 rounded-xl bg-white/10 backdrop-blur-lg border border-white/20 text-gray-200">1</div>
-          <div class="w-full h-60 bg-white p-5 rounded-xl">2</div>
-          <div class="w-full h-60 bg-white p-5 rounded-xl">3</div>
-          <div class="w-full h-60 bg-white p-5 rounded-xl">4</div>
+          <div v-for="n in 4" :key="n"
+            class="w-full h-60 bg-white p-5 rounded-xl text-center content-center bg-white/10 backdrop-blur-lg border border-white/20 text-gray-200">
+            <h2>Upcomming ...</h2>
+          </div>
+
         </div>
         <div class="my-10">
           <h2 class="text-3xl font-bold text-white">All Projects</h2>
           <hr class="text-white mt-2 w-40">
+          <div class="mt-10">
+            <div class="flex gap-3">
+              <div v-for="items in 3" :key="items"
+                class="w-full h-60 bg-white p-5 rounded-xl text-center content-center bg-white/10 backdrop-blur-lg border border-white/20 text-gray-200">
+                <h2>Upcomming ...</h2>
+              </div>
+            </div>
+          </div>
         </div>
         <div>
 
